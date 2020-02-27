37eefb4f3157f1fcfb3326465eee23479dc61c02
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-02-26 19:35:30 -0500

    Update startEXECRISE.sh

--- exercises/startEXECRISE.sh
+++ exercises/startEXECRISE.sh
@@ -2,6 +2,6 @@
 #remeber to . ./
 echo "what number exercise are you on?"
  read a
-  mkdir -p /home/chris/Documents/learn-python3-the-hard-way/exercises/e$a
-  vim /home/chris/Documents/learn-python3-the-hard-way/exercises/e$a/ex$a.py
-  cd /home/chris/Documents/learn-python3-the-hard-way/exercises/e$a
+  mkdir e$a
+  vim /e$a/ex$a.py
+  cd e$a

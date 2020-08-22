4417eb9d5c5794f63f37246a238ad7066b473a58
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-08-22 05:43:14 -0400

    Update and rename clickCat.py to clicker.py

--- clicker.py
+++ clicker.py
@@ -4,6 +4,6 @@ print('position mouse')
 input("r?")
 print(pyautogui.position())
 cx = int(input("enter x coords: "))
-cy = int(input("enter y coords "))
+cy = int(input("enter y coords: "))
 for i in range(int(input("enter number of clicks: "))):
     pyautogui.click(cx, cy)

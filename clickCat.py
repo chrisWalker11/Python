4e64ab36b396c2ce4218d666b02d9863baf3e38c
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-08-22 05:42:47 -0400

    Add files via upload

---
+++ clickCat.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+print('position mouse')
+input("r?")
+print(pyautogui.position())
+cx = int(input("enter x coords: "))
+cy = int(input("enter y coords "))
+for i in range(int(input("enter number of clicks: "))):
+    pyautogui.click(cx, cy)

40b6efedcce0eb232a473281820640fb485cc46f
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-09-19 04:05:27 -0400

    Update click.py

--- phoenixClicker/click.py
+++ phoenixClicker/click.py
@@ -1,10 +1,7 @@
-import pyautogui
-x1 = 987
-y1 = 968
-x2 = 986
-y2 = 864
+import pyautogui 
+from variables import *
 for i in range(int(input("enter number of clicks: "))):
-pyautogui.moveTo(x1, y1)
-pyautogui.click(x1, y1)
-pyautogui.moveTo(x2, y2)
-pyautogui.click(x2, y2)
+    pyautogui.moveTo(x1, y1)
+    pyautogui.click(x1, y1)
+    pyautogui.moveTo(x2, y2)
+    pyautogui.click(x2, y2)

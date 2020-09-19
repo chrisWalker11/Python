911b74802d76b9ea881ae4615b411e7b1fd15d67
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-09-19 04:05:01 -0400

    Update pos.py

--- phoenixClicker/pos.py
+++ phoenixClicker/pos.py
@@ -1,5 +1,21 @@
 import pyautogui
-while True:
-    input("enter when ready")
-    print(pyautogui.position())
-    print(" ")
+
+
+
+print("start with phoenix")
+input("press enter once over agent")
+print(pyautogui.position())
+px = input("input x coordinate for next photo: ")
+py = input("input y coordinate for next photo: ")
+with open("variables.py", "w") as f:
+    f.writelines("x1 = " + px + '\n')
+    f.writelines("y1 = " + py + '\n')
+
+
+print("put cursor  over agent confirm")
+input("press enter once over confirm)
+print(pyautogui.position())
+lx = input("input x coordinate for like: ")
+ly = input("input y coordinate for like: ")
+with open('variables.py', 'a') as f:
+    f.writelines("x2 = " +  lx + '\n')

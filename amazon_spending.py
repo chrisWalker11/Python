2909d941e770a53ea19c39bcd51da7c198b19b7b
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2021-07-13 07:53:04 -0400

    Add files via upload

---
+++ amazon_spending.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv('amazon.csv')
+df = df.fillna(0)
+df["Item Subtotal"] = df["Item Subtotal"].str.replace('$','').astype(float)
+total = df["Item Subtotal"].sum()
+print(total)
